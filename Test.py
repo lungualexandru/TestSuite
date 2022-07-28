2b7f91bc719ad07ad8043eb193f69cf2278ffe78
lungualexandru <67507624+lungualexandru@users.noreply.github.com>
2022-07-28 20:33:07 +0300

    Init
    Here there be dragons

---
+++ Test.py
@@ -0,0 +1,30 @@
+import time
+import pyvisa
+import numpy as np
+from matplotlib import pyplot as plt 
+rm = pyvisa.ResourceManager()
+rm.list_resources()
+inst=rm.open_resource('GPIB0::22::INSTR')
+inst.write("F0,1X")
+inst.write("Q1,0,5,1,2,100X")
+inst.write("N1X")
+inst.write("H0X")
+time.sleep(5)
+inst.write("G5,1,2X")
+querylist=(inst.query("Measure$")).replace("NSSWV","").replace("NMSWI","").split(",")
+data = []
+for i in range(0,len(querylist),2):
+    voltage= float(querylist[i])
+    current= float(querylist[i+1])
+    sweep=[voltage, current]
+    data+sweep
+for item in data:
+    print(item)
+
+plt.title("Voltage Sweep")
+plt.xlabel("Voltage")
+plt.ylabel("Current")
+plt.plot(querylist);
+plt.show()
+
+
