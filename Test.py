a21c8d18d9c0b63eb24a556dffd44b51f925f48b
anca <amocanas@wellesley.edu>
2022-07-28 20:44:38 +0300

    refactored raw data format

--- Test.py
+++ Test.py
@@ -1,24 +1,32 @@
 import time
 import pyvisa
 import numpy as np
-from matplotlib import pyplot as plt 
+from matplotlib import pyplot as plt
+
 rm = pyvisa.ResourceManager()
 rm.list_resources()
-inst=rm.open_resource('GPIB0::22::INSTR')
+inst = rm.open_resource('GPIB0::22::INSTR')
 inst.write("F0,1X")
 inst.write("Q1,0,5,1,2,100X")
 inst.write("N1X")
 inst.write("H0X")
 time.sleep(5)
 inst.write("G5,1,2X")
-querylist=(inst.query("Measure$")).replace("NSSWV","").replace("NMSWI","").split(",")
-data = []
-for i in range(0,len(querylist),2):
-    voltage= float(querylist[i])
-    current= float(querylist[i+1])
-    sweep=[voltage, current]
-    data+sweep
-for item in data:
+querylist = (inst.query("Measure$")).replace("NSSWV", "").replace("NMSWI", "").split(",")
+
+
+def get_voltage_and_current(rawData):
+    data = []
+    for i in range(0, len(querylist), 2):
+        voltage = float(querylist[i])
+        current = float(querylist[i + 1])
+        sweep = [voltage, current]
+        data + sweep
+    return data
+
+
+graphData = get_voltage_and_current(querylist)
+for item in graphData:
     print(item)
 
 plt.title("Voltage Sweep")
@@ -26,5 +34,3 @@ plt.xlabel("Voltage")
 plt.ylabel("Current")
 plt.plot(querylist);
 plt.show()
-
-
