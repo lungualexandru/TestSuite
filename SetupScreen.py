60a88ddba506db3af0ec98f7c16ea0a50065906f
Alex <lungualexandru43@gmail.com>
2022-07-31 20:33:22 +0300

    Totally F%4n Munted

--- SetupScreen.py
+++ SetupScreen.py
@@ -3,11 +3,20 @@ from tkinter import *
 import numpy as np
 
 rm = pyvisa.ResourceManager()
-listOfIntruments =np.asarray(rm.list_resources())
+listOfIntruments = np.asarray(rm.list_resources())
 listOfIntruments.flatten()
 master = Tk()
+
 variable = StringVar(master)
 variable.set(listOfIntruments[0])
-w=OptionMenu(master,variable,*listOfIntruments)
+
+w = OptionMenu(master, variable, *listOfIntruments)
 w.pack()
+
+def ok():
+    print ("value is:" + variable.get())
+
+button = Button(master, text="OK", command=ok)
+button.pack()
+
 mainloop()
