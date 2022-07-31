dce487fa83d5fc9896f0f42125ce6285b11e8f7e
Alex <lungualexandru43@gmail.com>
2022-07-31 17:03:25 +0300

    Totally F%4n Munted

--- SetupScreen.py
+++ SetupScreen.py
@@ -6,10 +6,10 @@ rm = pyvisa.ResourceManager()
 listOfIntruments =np.asarray(rm.list_resources())
 print("The following instruments are available", listOfIntruments)
 
-master = Tk()
-variable = StringVar(master)
-variable.set(Options[0])
-w = [OptionMenu(master, instr, *Options) for instr in listOfIntruments]
-[f.pack() for f in w]
+# master = Tk()
+# variable = StringVar(master)
+# variable.set(Options[0])
+# w = [OptionMenu(master, instr, *Options) for instr in listOfIntruments]
+# [f.pack() for f in w]
 
-mainloop()
+# mainloop()
