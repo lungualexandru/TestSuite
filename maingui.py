09902c6635a9b1d51a7088d83d8ff33fe1cb750b
Alex <lungualexandru43@gmail.com>
2022-08-02 11:49:00 +0300

    display current voltage

--- maingui.py
+++ maingui.py
@@ -4,39 +4,34 @@ import numpy as np
 import easy_scpi as scpi
 from time import time
 from tkinter import ttk
-def maingui(thermo_in,chip_id,chip_descr):
+
+
+def maingui(thermo_in, chip_id, chip_descr):
     swindow = tk.Tk()
     swindow.geometry('640x480')
     tk.Button(swindow, text='Quit', command=swindow.quit).grid(row=4, column=0, sticky=tk.W, pady=4)
     tk.Button(swindow, text='Show', command=swindow.quit).grid(row=4, column=1, sticky=tk.W, pady=4)
-    tk.Button(swindow, text="Done", command=swindow.quit).grid(row=4,column=3, sticky=tk.W, pady=4)
+    tk.Button(swindow, text="Done", command=swindow.quit).grid(row=4, column=3, sticky=tk.W, pady=4)
 
-    label1=tk.Label(swindow, text="loading").grid(row=5, column=0)
+    meas_voltage = open_ts(thermo_in)
+    volts_arr = []
+    def update_voltage():
+        vlt = measure_volts(meas_voltage)
+        swindow.after(1000, update_voltage)
+        volts_arr.append(float(vlt))
+        print("volts", volts_arr)
+        tk.Label(swindow, text="Current measured voltage is: {}".format(vlt)).grid(row=5, column=0)
+        return volts_arr
 
-
-
-    # swindow.after(1,open_ts,thermo_in,label1)
+    update_voltage()
     swindow.mainloop()
-    def Draw()
-        global text
-        frame = tk.frame(root, width=100, height=100, relief='solid',bd=1)
-        frame.place(x=10, y=10)
-        text=tk.Label(frame, text='Hello')
-        text.pack()
-    def Refresher():
-        global text
-        text.configure(text=time.asctime())
-        root.after(1000, Refresher())
-def open_ts(thermo_resource):
 
+
+def measure_volts(inst):
+    return inst.measure.voltage()
+
+
+def open_ts(thermo_resource):
     dmm = scpi.Instrument(thermo_resource)
     dmm.connect()
-    voltage = dmm.measure.voltage()
-    return voltage
-    # return tk.Label(window, text=voltage).grid(row=5, column=0)
-
-
-
-
-# if __name__ == '__maingui__':
-#     maingui()
+    return dmm
